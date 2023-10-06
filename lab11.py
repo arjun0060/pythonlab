e4d011519a4028e059fe1828d10c8fb0dae67206
arjun0060 <142004243+arjun0060@users.noreply.github.com>
2023-10-06 15:39:18 +0530

    Create lab11.py

---
+++ lab11.py
@@ -0,0 +1,12 @@
+import numpy as np
+a=np.arange(10)
+print(a)
+b=np.append(a,12)
+print(b)
+print("reverse=",np.flip(b))
+c=np.asfarray(b)
+print(c)
+maximum=np.max(b)
+minimum=np.min(b)
+print("maximum=",maximum)
+print("minimum=",minimum)
